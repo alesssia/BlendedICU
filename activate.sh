228108c40d9b4564bbc1b6c879ccac7ea85f14a6
Alessia Visconti <alessia.visconti@unito.it>
2025-07-02 14:48:29 +0200

    Script to create and activate py env, specified paths

---
+++ activate.sh
@@ -0,0 +1,1 @@
+. ./BlendedICU/bin/activate
---
+++ install.sh
@@ -0,0 +1,11 @@
+python3 -m venv BlendedICU
+. ./BlendedICU/bin/activate
+pip install numpy==1.26.3
+pip install pandas==2.2.0
+pip install pandas==2.2.0
+pip install pyarrow==11.0.0
+pip install seaborn==0.13
+pip install matplotlib==3.8.0
+pip install natsort==8.4.0
+pip install polars==0.20
+pip install chardet==5.2.0
